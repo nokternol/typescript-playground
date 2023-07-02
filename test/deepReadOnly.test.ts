aaeb98301174e92417253fb2a3d23eca765d2004
Mark Butterworth <nokternol@users.noreply.github.com>
2023-07-02 10:39:54 +0100

    prevent date mutation by replacing set methods on the prototype chain

--- test/deepReadOnly.test.ts
+++ test/deepReadOnly.test.ts
@@ -1,87 +1,93 @@
-import { deepReadonly, type DeepReadonly } from '../utils/deepReadOnly/deepReadOnly';
-import { expect } from 'chai';
+import {
+  deepReadonly,
+  type DeepReadonly,
+} from "../utils/deepReadOnly/deepReadOnly";
+import { expect } from "chai";
 
-describe('deepReadonly', () => {
-  it('should only accept objects', () => {
-    //non-object types
-    expect(deepReadonly.bind(null, 'value' as any)).to.throw;
-    expect(deepReadonly.bind(null, 1 as any)).to.throw;
-    expect(deepReadonly.bind(null, Symbol('key') as any)).to.throw;
-    expect(deepReadonly.bind(null, function() {} as any)).to.throw;
-    expect(deepReadonly.bind(null, true as any)).to.throw;
-    // empty values
-    expect(deepReadonly.bind(null, null as any)).to.throw;
-    expect(deepReadonly.bind(null, undefined as any)).to.throw;
+describe("deepReadonly", () => {
+  it("should only accept objects", () => {
+    // @ts-expect-error
+    expect(deepReadonly.bind(null, "value")).to.throw;
+    // @ts-expect-error
+    expect(deepReadonly.bind(null, 1)).to.throw;
+    // @ts-expect-error
+    expect(deepReadonly.bind(null, Symbol("key"))).to.throw;
+    // @ts-expect-error
+    expect(deepReadonly.bind(null, function () {})).to.throw;
+    // @ts-expect-error
+    expect(deepReadonly.bind(null, true)).to.throw;
+    // @ts-expect-error
+    expect(deepReadonly.bind(null, null)).to.throw;
+    // @ts-expect-error
+    expect(deepReadonly.bind(null, undefined)).to.throw;
   });
-  it('should gracefully handle object types', () => {
+  it("should gracefully handle object types", () => {
     const date = new Date();
     expect(deepReadonly(date)).to.equal(date);
 
     const array = new Array(100).map((_, i) => i);
     expect(deepReadonly(array)).to.equal(array);
-
   });
-  it('will return the same object', () => {
-    const input = { key: 'value' };
-    const result: DeepReadonly<typeof input> = deepReadonly(input)
-    expect(result).to.eql({ key: 'value' });
+  it("will return the same object", () => {
+    const input = { key: "value" };
+    const result: DeepReadonly<typeof input> = deepReadonly(input);
+    expect(result).to.eql({ key: "value" });
   });
-  it('will not allow re-assignment', () => {
-    const result = deepReadonly({ key: 'value' })
-    expect(Object.assign.bind(result, { key: 'another value'})).to.throw;
+  it("will not allow re-assignment", () => {
+    const result = deepReadonly({ key: "value" });
+    expect(Object.assign.bind(result, { key: "another value" })).to.throw;
   });
-  it('should not allow Object.assign', () => {
-    const result = deepReadonly({ arr: ['value1', 'value2', 'value3'] }) as any;
-    expect(result.arr.push.bind(result, 'value4')).to.throw;
+  it("should not allow Object.assign", () => {
+    const result = deepReadonly({ arr: ["value1", "value2", "value3"] }) as any;
+    expect(result.arr.push.bind(result, "value4")).to.throw;
   });
-  it('should not allow mutating array methods', () => {
-    const result = deepReadonly({ arr: ['value1', 'value2', 'value3'] }) as any;
+  it("should not allow mutating array methods", () => {
+    const result = deepReadonly({ arr: ["value1", "value2", "value3"] }) as any;
     expect(result.arr.reverse.bind(result)).to.throw;
   });
-  it('should not allow indexed assignment', () => {
+  it("should not allow indexed assignment", () => {
     const array = new Array(100).map((_, i) => i);
     const readOnlyArray = deepReadonly(array);
     readOnlyArray.forEach((_, i) => (readOnlyArray as any)[i]++);
     expect(readOnlyArray).deep.equal(array);
   });
-  it('should protect deeply nested properties', () => {
+  it("should protect deeply nested properties", () => {
     const originalValue = 5;
-    const input = { root: { [1]: [{ [2]: { [3]: originalValue }}]}};
+    const input = { root: { [1]: [{ [2]: { [3]: originalValue } }] } };
     const adjustedValue = 6;
     input.root[1][0][2][3] = adjustedValue;
     expect(input.root[1][0][2][3]).to.equal(adjustedValue);
-    
+
     const immutable = deepReadonly(input);
     try {
       /**
-       * cast to any as TS knows the assignment is readonly 
+       * cast to any as TS knows the assignment is readonly
        * has to be wrapped in try/catch as the assignment throws a runtime error
        */
       (immutable.root[1][0][2][3] as any) = originalValue;
     } catch {}
-    
+
     expect(immutable.root[1][0][2][3]).to.equal(adjustedValue);
   });
-  it('should prevent assignment of a new date to a nested property', () => {
+  it("should prevent assignment of a new date to a nested property", () => {
     const originalValue = new Date(1990, 0, 1);
-    const input = { root: { [1]: [{ [2]: { [3]: originalValue }}]}};
+    const input = { root: { [1]: [{ [2]: { [3]: originalValue } }] } };
     const adjustedValue = new Date(1980, 0, 1);
     input.root[1][0][2][3] = adjustedValue;
     expect(input.root[1][0][2][3]).to.equal(adjustedValue);
-    
+
     const immutable = deepReadonly(input);
     try {
       /**
-       * cast to any as TS knows the assignment is readonly 
+       * cast to any as TS knows the assignment is readonly
        * has to be wrapped in try/catch as the assignment throws a runtime error
        */
       (immutable.root[1][0][2][3] as any) = originalValue;
     } catch {}
-    
+
     expect(immutable.root[1][0][2][3]).to.equal(adjustedValue);
   });
-
-  it('should prevent mutation of Date objects via their set* methods', () => {
+  it("should prevent mutation of Date objects via their set* methods", () => {
     const date = new Date(1990, 0, 1);
     const readOnlyDate = deepReadonly(date);
     readOnlyDate.setFullYear(1991);
--- utils/deepReadOnly/deepReadOnly.ts
+++ utils/deepReadOnly/deepReadOnly.ts
@@ -1,32 +1,47 @@
-export type DeepReadonly<T> =
-    T extends (infer R)[] ? DeepReadonlyArray<R> :
-    T extends Function ? T :
-    T extends object ? DeepReadonlyObject<T> :
-    T;
+export type DeepReadonly<T> = T extends (infer R)[]
+  ? DeepReadonlyArray<R>
+  : T extends Function
+  ? T
+  : T extends object
+  ? DeepReadonlyObject<T>
+  : T;
 
 interface DeepReadonlyArray<T> extends ReadonlyArray<DeepReadonly<T>> {}
 
 type DeepReadonlyObject<T> = {
-    readonly [P in keyof T]: DeepReadonly<T[P]>;
+  readonly [P in keyof T]: DeepReadonly<T[P]>;
 };
+
+const dateMutatorMethodKeys = ["setDate","setFullYear","setHours","setMilliseconds","setMinutes","setMonth","setSeconds","setTime","setUTCDate","setUTCFullYear","setUTCHours","setUTCMilliseconds","setUTCMinutes","setUTCMonth","setUTCSeconds","setYear"];
+
+type ObjectType<T> = T extends object ? T : never;
 
 /**
  * Protect the runtime code from mutation
- * caveats: JS Dates cannot be frozen with Object.freeze but they are also not easy to accidentally 
+ * caveats: JS Dates cannot be frozen with Object.freeze but they are also not easy to accidentally
  * mutate date.setMonth() is quite deliberate
- * @param input 
- * @returns 
+ * @param input
+ * @returns
  */
-export function deepReadonly<T extends object>(input: T): DeepReadonly<T> {
-    if (typeof input !== 'object') throw new Error('deepReadonly is for use with objects');
+export function deepReadonly<T>(input: ObjectType<T>): DeepReadonly<T> {
+  if (typeof input !== "object")
+    throw new Error("deepReadonly is for use with objects");
+  const indexer = input as { [key: string]: unknown };
 
-    Object.keys(input).forEach((value) => {
-        const key: string & keyof typeof input = value as string & keyof typeof input;
-        if (typeof input[key] === 'object') {
-            // work out how to remove these any usages later
-            (input[key] as any) = deepReadonly(input[key] as any);
-        }
+  if (input instanceof Date) {
+    dateMutatorMethodKeys.forEach(key => {
+      indexer[key] = function(){};
     });
-    return Object.freeze(input) as DeepReadonly<T>;
+    //replace all set* methods with no-op functions
+    //input = immutableDate(input);
+  } else {
+    Object.keys(input).forEach((key) => {
+      if (typeof indexer[key] === "object") {
+        
+        // work out how to remove these any usages later
+        indexer[key] = deepReadonly(indexer[key] as object);
+      }
+    });
+  }
+  return Object.freeze(input) as DeepReadonly<T>;
 }
-
